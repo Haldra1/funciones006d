11f7e38cb76b1f65b041dbd90630fd3dc243d6ed
Haldra <camil.valenzuelao@duocuc.cl>
2026-06-10 17:07:23 -0400

    cree el archivo funciones1.py

---
+++ funciones1.py
@@ -0,0 +1,29 @@
+#Funciones
+def datos_productos(nombre, stock, precio): #No importa el orden de los parametros
+    print("====================================")
+    print(f"||Nombre del producto {nombre} ||")
+    print(f"||Precio del producto {precio} ||")
+    print(f"||Stock del producto {stock} ||")
+    print("====================================")
+    
+#Codigo principal
+nombre=input("Ingrese el nombre del producto: \n")
+while True:
+    try:
+        precio=int(input("Ingrese el precio del producto: \n"))
+        if precio<=0:
+            print("Error: Debe ser un número positivo ")
+        else:
+            break
+    except ValueError:
+        print("Error: Ingrese numeros")
+while True:
+    try:
+        stock=int(input("Ingrese el precio del producto: \n"))
+        if precio< 0:
+            print("Error: El precio ingresado es incorrecto ")
+        else:
+            break
+    except ValueError:
+        print("Error: Ingrese numeros")
+datos_productos(nombre, stock, precio) #Se deben enviar los parametros en el orden exacto que los declare al crear la funcion
